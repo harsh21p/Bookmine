a975cbaa71e9edefd95be1d0177be125312d0ffa
harsh21p <harshadpachore21@gmail.com>
2021-05-14 17:20:20 +0530

    ADDED

--- app/src/main/java/com/example/bookmine/MainActivity.java
+++ app/src/main/java/com/example/bookmine/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.bookmine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mycode
+        editText = findViewById(R.id.editTextTextPersonName);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Wait For a Min.",Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int a = Integer.parseInt(s);
+
+
+            }
+        });
+
+
+
     }
 }
