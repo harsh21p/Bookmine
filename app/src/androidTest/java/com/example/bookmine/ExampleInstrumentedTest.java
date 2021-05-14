89b5874b356aaf3359dd16a6956151bc5d3d8803
harsh21p <harshadpachore21@gmail.com>
2021-05-14 09:40:30 +0530

    start...

--- app/src/androidTest/java/com/example/bookmine/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bookmine/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.testapp;
+package com.example.bookmine;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.testapp", appContext.getPackageName());
+        assertEquals("com.example.bookmine", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/bookmine/ExampleUnitTest.java
+++ app/src/test/java/com/example/bookmine/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.testapp;
+package com.example.bookmine;
 
 import org.junit.Test;
 
