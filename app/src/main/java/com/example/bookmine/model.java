6c3bdf4cd2eb0ea1af3dac265d464cb0fe59e4c1
harsh21p <harshadpachore21@gmail.com>
2021-05-19 11:49:39 +0530

    model done

---
+++ app/src/main/java/com/example/bookmine/model.java
@@ -0,0 +1,21 @@
+package com.example.bookmine;
+
+public class model {
+    String name;
+
+    public model() {
+
+    }
+
+    public model(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
