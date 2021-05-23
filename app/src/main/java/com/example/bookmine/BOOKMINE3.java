7a20228595a887298425726545f37cdb07a8868c
harsh21p <harshadpachore21@gmail.com>
2021-05-23 17:07:39 +0530

    done first task

--- app/src/main/java/com/example/bookmine/BOOKMINE3.java
+++ app/src/main/java/com/example/bookmine/BOOKMINE3.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +14,7 @@ import com.bumptech.glide.Glide;
 
 public class BOOKMINE3 extends AppCompatActivity {
     TextView secondPageSearchContent3;
-    String title,coverlink,author,category,noofpages,year,amazonredirecturl,authorlink,fivestarrating,fourstarrating,booklinks,onestarrating,ratingcount,reviewrating,worldcatredirectlink;
+    String twostarrating,threestarrating,title,coverlink,author,category,noofpages,year,amazonredirecturl,authorlink,fivestarrating,fourstarrating,booklinks,onestarrating,ratingcount,reviewrating,worldcatredirectlink;
     public static final String EXTRA_TEXT_LAST = "com.example.bookmine.extra.last";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +41,15 @@ public class BOOKMINE3 extends AppCompatActivity {
         ratingcount = intent1.getStringExtra(MainActivity2.EXTRA_TEXT13);
         reviewrating = intent1.getStringExtra(MainActivity2.EXTRA_TEXT14);
         worldcatredirectlink = intent1.getStringExtra(MainActivity2.EXTRA_TEXT15);
+        twostarrating = intent1.getStringExtra(MainActivity2.EXTRA_TEXT17);
+        threestarrating = intent1.getStringExtra(MainActivity2.EXTRA_TEXT16);
 
-        setDetailspage3(title,coverlink,author,category,noofpages,year,amazonredirecturl,authorlink,fivestarrating,fourstarrating,booklinks,onestarrating,ratingcount,reviewrating,worldcatredirectlink);
+        setDetailspage3(title,coverlink,author,category,noofpages,year,amazonredirecturl,authorlink,fivestarrating,fourstarrating,booklinks,onestarrating,ratingcount,reviewrating,worldcatredirectlink,twostarrating,threestarrating);
 
 
     }
 
-    public void setDetailspage3( String bookname, String coverlink, String authorname, String category, String noofpages, String year, String amazon_redirect_url, String author_link, String five_star_rating, String four_star_rating, String booklinks, String onr_star_rating, String rating_count, String review_count, String worldcat_redirect_link)
+    public void setDetailspage3( String bookname, String coverlink, String authorname, String category, String noofpages, String year, String amazon_redirect_url, String author_link, String five_star_rating, String four_star_rating, String booklinks, String onr_star_rating, String rating_count, String review_count, String worldcat_redirect_link,String twostarrating,String threestarrating)
     {
 
         TextView book_name = findViewById(R.id.booknametext1);
@@ -53,12 +57,63 @@ public class BOOKMINE3 extends AppCompatActivity {
         TextView Category_name = findViewById(R.id.categorytext1);
         TextView year_published = findViewById(R.id.yeartext1);
         ImageView image = findViewById(R.id.imageView);
+        ImageView amazon = findViewById(R.id.amazonlink);
+        ImageView goodread =findViewById(R.id.goodreadlink);
+        ImageView authorlink =findViewById(R.id.authorlink);
+        //TextView noofpages1 = findViewById(R.id.noofpagestext1);
+        TextView ratingones1 = findViewById(R.id.rating1);
+        TextView ratingones2 = findViewById(R.id.rating2);
+        TextView ratingones3 = findViewById(R.id.rating3);
+        TextView ratingones4 = findViewById(R.id.rating4);
+        TextView ratingones5 = findViewById(R.id.rating5);
+        TextView overallrating = findViewById(R.id.overallrating);
 
+        String overall="4.7";//perfom operation
         book_name.setText(bookname);
         author_name.setText(authorname);
         Category_name.setText(category);
         year_published.setText(year);
+        //noofpages1.setText(noofpages);
+        ratingones1.setText(onr_star_rating);
+        ratingones2.setText(twostarrating);
+        ratingones3.setText(threestarrating);
+        overallrating.setText(overall);
+        ratingones4.setText(four_star_rating);
+        ratingones5.setText(five_star_rating);
+
         Glide.with(getApplicationContext()).load(coverlink).into(image);
+
+        amazon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(amazon_redirect_url));
+                startActivity(intent);
+            }
+        });
+        goodread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(booklinks));
+                startActivity(intent);
+            }
+        });
+        authorlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(booklinks));
+                startActivity(intent);
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/bookmine/MainActivity2.java
+++ app/src/main/java/com/example/bookmine/MainActivity2.java
@@ -40,6 +40,8 @@ public class MainActivity2 extends AppCompatActivity {
     public static final String EXTRA_TEXT13= "Extra.search.for.third.page.category10";
     public static final String EXTRA_TEXT14= "Extra.search.for.third.page.category11";
     public static final String EXTRA_TEXT15= "Extra.search.for.third.page.category12";
+    public static final String EXTRA_TEXT16= "Extra.search.for.third.page.category13";
+    public static final String EXTRA_TEXT17= "Extra.search.for.third.page.category14";
 
 
 
@@ -130,21 +132,10 @@ public class MainActivity2 extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
 
-                            openActivity3(v, book1.getTitle(), book1.getCover_link(), book1.getAuthor(), book1.getGenre_and_votes(), book1.getNumber_of_pages(), book1.getYear_published(), book1.getAmazon_redirect_link(), book1.getAuthor_link(), book1.getFive_star_ratings(), book1.getFour_star_ratings(), book1.getBooklinks(), book1.getOne_star_ratings(), book1.getRating_count(), book1.getReview_count(), book1.getWorldcat_redirect_link());
+                            openActivity3(v, book1.getTitle(), book1.getCover_link(), book1.getAuthor(), book1.getGenre_and_votes(), book1.getNumber_of_pages(), book1.getYear_published(), book1.getAmazon_redirect_link(), book1.getAuthor_link(), book1.getFive_star_ratings(), book1.getFour_star_ratings(), book1.getBooklinks(), book1.getOne_star_ratings(), book1.getRating_count(), book1.getReview_count(), book1.getWorldcat_redirect_link(),book1.getThree_star_ratings(),book1.getTwo_star_ratings());
 
                         }
                     });
-                        usersViewHolder.setDetails(getApplicationContext(), book1.getTitle(), book1.getCover_link(), book1.getAuthor(), book1.getGenre_and_votes(), book1.getNumber_of_pages(), book1.getYear_published(), book1.getAmazon_redirect_link(), book1.getAuthor_link(), book1.getFive_star_ratings(), book1.getFour_star_ratings(), book1.getBooklinks(), book1.getOne_star_ratings(), book1.getRating_count(), book1.getReview_count(), book1.getWorldcat_redirect_link());
-
-
-                        usersViewHolder.mView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                                openActivity3(v, book1.getTitle(), book1.getCover_link(), book1.getAuthor(), book1.getGenre_and_votes(), book1.getNumber_of_pages(), book1.getYear_published(), book1.getAmazon_redirect_link(), book1.getAuthor_link(), book1.getFive_star_ratings(), book1.getFour_star_ratings(), book1.getBooklinks(), book1.getOne_star_ratings(), book1.getRating_count(), book1.getReview_count(), book1.getWorldcat_redirect_link());
-
-                            }
-                        });
                     }
                  };
 
@@ -176,7 +167,7 @@ public class MainActivity2 extends AppCompatActivity {
         }
     }
 
-    public void openActivity3(View view,String bookname,String coverlink,String authorname, String category, String noofpages,String year,String amazon_redirect_url,String author_link,String five_star_rating,String four_star_rating,String booklinks,String onr_star_rating,String rating_count,String review_count,String worldcat_redirect_link)
+    public void openActivity3(View view,String bookname,String coverlink,String authorname, String category, String noofpages,String year,String amazon_redirect_url,String author_link,String five_star_rating,String four_star_rating,String booklinks,String onr_star_rating,String rating_count,String review_count,String worldcat_redirect_link,String three_star_rating,String toe_star_rating)
     {
         Toast.makeText(MainActivity2.this,"Opening "+bookname,Toast.LENGTH_SHORT).show();
         Intent intent1 = new Intent(this, BOOKMINE3.class);
@@ -195,6 +186,8 @@ public class MainActivity2 extends AppCompatActivity {
         intent1.putExtra(EXTRA_TEXT13,rating_count);
         intent1.putExtra(EXTRA_TEXT14,review_count);
         intent1.putExtra(EXTRA_TEXT15,worldcat_redirect_link);
+        intent1.putExtra(EXTRA_TEXT16,three_star_rating);
+        intent1.putExtra(EXTRA_TEXT17,toe_star_rating);
         startActivity(intent1);
     }
 
