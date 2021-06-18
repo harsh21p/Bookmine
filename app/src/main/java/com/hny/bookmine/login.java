c6e0fe93b9386b1f7050007823e04c952c1b98ea
yashmahaja <yashmahajan0251@gmail.com>
2021-06-18 10:27:36 +0530

    Validating Forms & Authentication Completed

--- app/src/main/java/com/hny/bookmine/login.java
+++ app/src/main/java/com/hny/bookmine/login.java
@@ -1,24 +1,41 @@
 package com.hny.bookmine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
+
+import static android.content.ContentValues.TAG;
 
 public class login extends AppCompatActivity {
 Button callsignup,login_button;
 ImageView image;
 TextInputLayout username, password;
 TextView sloganText,logoText;
+FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +49,7 @@ TextView sloganText,logoText;
         logoText = findViewById(R.id.logo_name);
         sloganText = findViewById(R.id.slogan_name);
         password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
         login_button = findViewById(R.id.login_btn);
 
         callsignup.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +57,72 @@ TextView sloganText,logoText;
             public void onClick(View v) {
                 Intent intent = new Intent(login.this,Signup.class);
                 startActivity(intent);
-
             }
         });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginme(v);
+            }
+        });
+
     }
+
+    private void loginme(View view){
+
+        if(!validatePassword() | !validateUsername()){
+            return;
+        }
+        String username1=username.getEditText().getText().toString();
+        String password1 = password.getEditText().getText().toString();
+        mAuth.signInWithEmailAndPassword(username1, password1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(login.this,MainActivity.class);
+
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(login.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+    }
+    private Boolean validateUsername(){
+        String val = username.getEditText().getText().toString();
+        if(val.isEmpty()){
+            username.setError("Field cannot be empty");
+            return false;
+        }
+        else{
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
 }
